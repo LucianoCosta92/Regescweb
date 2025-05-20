4db60a844d90446a92837fea7c770c0ccc2e812f
Luciano Costa <lucianocostax@gmail.com>
2025-05-20 15:54:27 -0300

    2º commit - métodos de add, delete, edit

--- regescweb/src/main/java/br/com/luciano/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/luciano/regescweb/controllers/ProfessorController.java
@@ -1,12 +1,15 @@
 package br.com.luciano.regescweb.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.luciano.regescweb.dto.ProfessorDTO;
@@ -16,37 +19,98 @@ import br.com.luciano.regescweb.repositories.ProfessorRepository;
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping(value = "/professores")
 public class ProfessorController {
-	
+
 	@Autowired
 	private ProfessorRepository professorRepository;
 
-	@GetMapping("/professores")
+	@GetMapping("")
 	public ModelAndView index() {
 		List<Professor> professores = this.professorRepository.findAll();
 		ModelAndView mv = new ModelAndView("professores/index");
 		mv.addObject("professores", professores);
 		return mv;
 	}
-	
-	@GetMapping("/professores/new")
-	public ModelAndView formulario() {
+
+	@GetMapping("/new")
+	public ModelAndView formulario(ProfessorDTO request) {
 		ModelAndView mv = new ModelAndView("professores/new");
-		mv.addObject("statusProfessor", StatusProfessor.values());
+		mv.addObject("listaStatusProfessor", StatusProfessor.values());
 		return mv;
 	}
-	
-	@PostMapping("/professores")
-	public String create(@Valid ProfessorDTO request, BindingResult result) {
+
+	@PostMapping("")
+	public ModelAndView create(@Valid ProfessorDTO request, BindingResult result) {
 		if (result.hasErrors()) {
-			System.out.println("ERRO:");
-			return "redirect:/professores/new";
+			ModelAndView mv = new ModelAndView("professores/new");
+			mv.addObject("listaStatusProfessor", StatusProfessor.values());
+			return mv;
 		} else {
 			Professor professor = request.toProfessor();
 			this.professorRepository.save(professor);
-			return "redirect:/professores";
+			return new ModelAndView("redirect:/professores/" + professor.getId());
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView show(@PathVariable Long id) {
+		Optional<Professor> optional = this.professorRepository.findById(id);
+		if (optional.isPresent()) {
+			Professor professor = optional.get();
+			ModelAndView mv = new ModelAndView("professores/show");
+			mv.addObject("professor", professor);
+			return mv;
+		} else {
+			return new ModelAndView("redirect:/professores");
+		}
+	}
+
+	@GetMapping("/{id}/edit")
+	public ModelAndView edit(@PathVariable Long id, ProfessorDTO request) {
+		Optional<Professor> optional = this.professorRepository.findById(id);
+		if (optional.isPresent()) {
+			Professor professor = optional.get();
+			request.fromProfessor(professor);
+
+			ModelAndView mv = new ModelAndView("professores/edit");
+			mv.addObject("professorId", professor.getId());
+			mv.addObject("listaStatusProfessor", StatusProfessor.values());
+			return mv;
+		} else {
+			return new ModelAndView("redirect:/professores");
+		}
+	}
+
+	@PostMapping("/{id}")
+	public ModelAndView update(@PathVariable Long id, @Valid ProfessorDTO request, BindingResult result) {
+		if (result.hasErrors()) {
+			ModelAndView mv = new ModelAndView("professores/edit");
+			mv.addObject("professorId", id);
+			mv.addObject("listaStatusProfessor", StatusProfessor.values());
+			return mv;
+		} else {
+			Optional<Professor> optional = this.professorRepository.findById(id);
+			if (optional.isPresent()) {
+				Professor professor = request.toProfessor(optional.get());
+				this.professorRepository.save(professor);
+				
+				return new ModelAndView("redirect:/professores/" + professor.getId());
+			} else {
+				return new ModelAndView("redirect:/professores");
+			}
 		}
 	}
 	
-	
+	@GetMapping("/{id}/delete")
+	public String delete(@PathVariable Long id) {
+		try {
+			this.professorRepository.deleteById(id);
+			return "redirect:/professores";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/professores";
+	}
+
 }
--- regescweb/src/main/java/br/com/luciano/regescweb/dto/ProfessorDTO.java
+++ regescweb/src/main/java/br/com/luciano/regescweb/dto/ProfessorDTO.java
@@ -63,7 +63,19 @@ public class ProfessorDTO {
 		return professor;
 	}
 	
+	public Professor toProfessor(Professor professor) {
+		professor.setNome(this.nome);
+		professor.setSalario(this.salario);
+		professor.setStatusProfessor(this.statusProfessor);
+		
+		return professor;
+	}
 	
+	public void fromProfessor(Professor professor) {
+		this.nome = professor.getNome();
+		this.salario = professor.getSalario();
+		this.statusProfessor = professor.getStatusProfessor();
+	}
 	
 	
 	
